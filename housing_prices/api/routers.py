1fc3e18586bee2af707ea3a546a60360448e1e6f
Jonáš Petrovský <jontesek@gmail.com>
2026-01-27 10:58:52 +0100

    feat: Implement predict endpoint

--- housing_prices/api/routers.py
+++ housing_prices/api/routers.py
@@ -1,15 +1,20 @@
 from fastapi import APIRouter
 
-from .schemas import PredictInput, PredictResponse
+from housing_prices.predict import load_model, predict_price, prepare_input_df
+
+from .schemas import PredictRequest, PredictResponse
 
 router = APIRouter()
 
 
 @router.get("/health", response_model=dict)
-def get_sites():
+def health():
     return {"status": "ok"}
 
 
-@router.post("/predict", response_model=PredictResponse)
-def get_site(input_json: PredictInput):
-    pass
+@router.post("/predict-price", response_model=PredictResponse)
+def predict(request_json: PredictRequest):
+    model = load_model("model.joblib")
+    input_df = prepare_input_df(request_json.model_dump())
+    price = predict_price(model, input_df)
+    return PredictResponse(house_price=price)
--- housing_prices/api/schemas.py
+++ housing_prices/api/schemas.py
@@ -1,9 +1,36 @@
+from typing import Literal
+
 from pydantic import BaseModel
 
+OceanProximity = Literal["NEAR BAY", "<1H OCEAN", "INLAND", "NEAR OCEAN", "ISLAND"]
 
-class PredictInput(BaseModel):
-    pass
+
+class PredictRequest(BaseModel):
+    longitude: float
+    latitude: float
+    housing_median_age: float
+    total_rooms: float
+    total_bedrooms: float
+    population: float
+    households: float
+    median_income: float
+    ocean_proximity: OceanProximity
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "longitude": -122.64,
+                "latitude": 38.01,
+                "housing_median_age": 36.0,
+                "total_rooms": 1336.0,
+                "total_bedrooms": 258.0,
+                "population": 678.0,
+                "households": 249.0,
+                "median_income": 5.5789,
+                "ocean_proximity": "NEAR OCEAN",
+            }
+        }
 
 
 class PredictResponse(BaseModel):
-    pass
+    house_price: float
